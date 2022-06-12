b1333d5dafbb4f4f64cd0421913ec762784c5ff3
Woo Joo Chae <enaenen@naver.com>
2022-06-13 02:08:24 +0900

    libft

--- dup/libft/libft.h
+++ dup/libft/libft.h
@@ -2,9 +2,15 @@
 # define LIBFT_H
 
 # include <unistd.h>
+# include <stdlib.h>
 
 size_t	ft_strlen(const char *str);
 size_t	ft_putstr_fd(const char *str, int fd);
-
+char	**ft_split(char const *str, char c);
+void	*ft_memset(void *b, int c, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strntrim(char *str, char *set, int n);
+char	*ft_strtrim(char const *str, char const *set);
+char	*ft_substr(const char *str, size_t start, size_t len);
 
 #endif
