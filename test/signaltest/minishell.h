796c85f959399218b3f1e23b9d091c28e9083001
Woo Joo Chae <enaenen@naver.com>
2022-06-24 22:52:19 +0900

    signal test

---
+++ test/signaltest/minishell.h
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wchae <wchae@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/11 17:08:37 by wchae             #+#    #+#             */
+/*   Updated: 2022/06/21 20:01:56 by wchae            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/errno.h>
+# include <string.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <signal.h>
+# include <dirent.h>
+# include <curses.h>
+# include "libft.h"
+
+# define ERROR -1
+
+int g_status;
+
+typedef struct s_list
+{
+	char			*data;
+	struct s_list	*next;
+}					t_list;
+
+typedef struct s_env
+{
+	char 			*key;
+	char 			*value;
+	int				print_check;
+	struct s_env	*next;
+}	t_env;
+
+typedef struct s_set
+{
+	struct termios	org_term;
+	struct termios	new_term;
+	int				org_stdin;
+	int				org_stdout;
+}	t_set;
+
+typedef struct s_proc
+{
+	t_env	*env_list;
+	t_list	*data;
+	t_list	*cmd;
+	t_list	*limiter;
+	int		infile;
+	int		outfile;
+	int		pipe_flag;
+	int		status;
+}	t_proc;
+
+void	ft_sig_handler(int status);
+
+
+
+#endif
---
+++ test/signaltest/srcs/handler.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handler.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wchae <wchae@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/12 15:13:14 by wchae             #+#    #+#             */
+/*   Updated: 2022/06/21 19:59:12 by wchae            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_replace_line(int var_stat)
+{
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+	g_status = var_stat;
+}
+
+void	ft_sig_handler(int status)
+{
+	pid_t	pid;
+
+	pid = waitpid(-1, 0, WNOHANG);
+	if (status == SIGINT)
+	{
+		if(pid == -1)
+		{
+			write(1, "\n", 1);
+			ft_replace_line(1);
+		}
+		else
+		{
+			write(1, "\n", 1);
+			g_status = 130;
+		}
+	}
+	// else if (status == SIGQUIT && pid != -1)
+	// {
+	// 	write(1, "Quit: 3\n", 8);
+	// 	g_status = 131;
+	// }
+	else
+		ft_replace_line(131);
+}
