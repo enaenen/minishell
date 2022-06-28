47ab3d35dcc3e3fe69a1d5319546f6b445bb8b9f
Woo Joo Chae <enaenen@naver.com>
2022-06-28 19:58:55 +0900

    strjoin fix

--- dup/libft/ft_strjoin.c
+++ dup/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: wchae <wchae@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 16:32:03 by wchae             #+#    #+#             */
-/*   Updated: 2022/06/12 16:33:46 by wchae            ###   ########.fr       */
+/*   Updated: 2022/06/28 17:22:35 by wchae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,15 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*rt;
-	int		i;
-	int		j;
+	char	*ret;
+	size_t	s1_len;
+	size_t	s2_len;
 
-	j = 0;
-	i = 0;
-	if (!s1 || !s2)
-		return (0);
-	rt = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!rt)
-		return (0);
-	while (s1[i])
-	{
-		rt[i] = s1[i];
-		i++;
-	}
-	while (s2[j])
-		rt[i++] = s2[j++];
-	rt[i] = 0;
-	s1 = 0;
-	return (rt);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	ret = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	ft_memcpy(ret, s1, s1_len);
+	ft_memcpy(ret + s1_len, s2, s2_len);
+	ret[s1_len + s2_len] = '\0';
+	return (ret);
 }
