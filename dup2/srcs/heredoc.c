371afd1337ad174a194dc1a6515c4fc6a82d2cc8
Woo Joo Chae <enaenen@naver.com>
2022-07-02 14:16:21 +0900

    add

---
+++ dup2/srcs/heredoc.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wchae <wchae@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 23:19:21 by wchae             #+#    #+#             */
+/*   Updated: 2022/06/30 21:48:48 by wchae            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+static char	*ft_merge_str(char *line, char buf)
+{
+	int		size;
+	char	*str;
+	int		i;
+
+	size = ft_strlen(line);
+	str = (char *)malloc(sizeof(char) * (size + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (line[i] != 0)
+	{
+		str[i] = line[i];
+		i++;
+	}
+	free(line);
+	str[i++] = buf;
+	str[i] = '\0';
+	return (str);
+}
+
+static int	get_next_line(char **line)
+{
+	char	buf;
+	int		ret;
+
+	*line = (char *)malloc(1);
+	if (*line == NULL)
+		return (-1);
+	(*line)[0] = 0;
+	ret = read(0, &buf, 1);
+	while (buf != '\n' && buf != '\0')
+	{
+		*line = ft_merge_str(*line, buf);
+		if (*line == 0)
+			return (-1);
+		ret = read(0, &buf, 1);
+	}
+	if (buf == '\n')
+		return (1);
+	return (0);
+}
+
+static void	print_line(char *line, char *limiter, int fd)
+{
+	if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0)
+		exit(0);
+	write(1, "> ", 2);
+	write(fd, line, ft_strlen(line));
+	write(fd, "\n", 1);
+}
+
+static int	ft_heredoc(char *limiter)
+{
+	char	*line;
+	int		fd[2];
+	pid_t	pid;
+	
+	// signal(SIGQUIT, SIG_IGN);
+	signal(SIGQUIT, &sig_readline);
+	signal(SIGINT, &sig_here_doc_child);
+	if (pipe(fd) == -1)
+		return (error_msg("pipe"));
+	pid = fork();
+	if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		close(fd[0]);
+		write(1, "> ", 2);
+		while (get_next_line(&line))
+			print_line(line, limiter, fd[1]);
+		close(fd[1]);
+		g_status = 0;
+	}
+	else if (0 < pid)
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		waitpid(pid, 0, 0);
+		close(fd[0]);
+	}
+	else
+		return (error_msg("fork"));
+	return (TRUE);
+}
+
+void	process_heredoc(t_list *token)
+{
+	int	org_stdin;
+
+	org_stdin = dup(STDIN_FILENO);
+	while (token)
+	{
+		if (ft_strncmp(token->data, "<<", 3) == 0)
+		{
+			dup2(org_stdin, STDIN_FILENO);
+			ft_heredoc(token->next->data);
+			token = token->next;
+		}
+		token = token->next;
+	}
+}
---
+++ dup2/srcs/lst_utils.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wchae <wchae@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 23:51:29 by wchae             #+#    #+#             */
+/*   Updated: 2022/06/28 15:26:26 by wchae            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int		ft_lstsize(t_list *lst)
+{
+	int	i;
+
+	i = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
+}
+
+void	env_lstadd_back(t_env **lst, t_env *new, char *key, char *value)
+{
+	t_env	**phead;
+
+	new = (t_env *)malloc(sizeof(t_env));
+	new->key = key;
+	new->value = value;
+	new->print_check = 0;
+	phead = lst;
+	while (*phead)
+		phead = &(*phead)->next;
+	new->next = *phead;
+	*phead = new;
+}
+
+t_list	*ft_lstnew(void *data)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->data = data;
+	new->next = NULL;
+	return (new);
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*next;
+
+	if (!lst || !*lst || !del)
+		return ;
+	while (*lst)
+	{
+		next = (*lst)->next;
+		del((*lst)->data);
+		(*lst)->data = NULL;
+		free(*lst);
+		*lst = NULL;
+		*lst = next;
+	}
+	*lst = NULL;
+}
