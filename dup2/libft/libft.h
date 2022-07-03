52f0559e417a254c0fd9be088e913bafd7aba69a
Woo Joo Chae <enaenen@naver.com>
2022-07-04 04:11:54 +0900

    str buffer add

--- dup2/libft/libft.h
+++ dup2/libft/libft.h
@@ -4,6 +4,13 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_buffer
+{
+	char	*str;
+	size_t	len;
+	size_t	size;
+}	t_buffer;
+
 size_t	ft_strlen(const char *str);
 size_t	ft_putstr_fd(const char *str, int fd);
 char	**ft_split(char const *str, char c);
@@ -26,5 +33,10 @@ void	ft_putstr(char *str);
 int		ft_atoi(const char *str);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	ft_free_split(char **arr);
-
+/* BUFFER*/
+t_buffer	*create_buf(void);
+char	*add_char(t_buffer *buf, char c);
+char	*add_str(t_buffer *buf, char *str);
+char	*put_str(t_buffer *buf);
+void	del_buf(t_buffer *buf);
 #endif
